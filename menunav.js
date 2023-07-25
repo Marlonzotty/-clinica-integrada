63ce42873af8ca9cc07058b68ec6eb20609af4c6
Marlonzotty <112110890+Marlonzotty@users.noreply.github.com>
2023-07-25 18:25:51 -0300

    commite 2

---
+++ menunav.js
@@ -0,0 +1,28 @@
+var menu = document.querySelector('nav ul');
+var menuBar = document.querySelector('nav .menu-icon');
+var iconMenu = document.querySelector('nav .menu-icon img');
+
+menuBar.addEventListener('click', function () {
+
+    if (iconMenu.getAttribute("src") == '/img/icons8-cardápio-50.png') {
+        iconMenu.setAttribute("src", "/img/icons8-cardápio-50.png");
+    } else {
+        iconMenu.setAttribute("src", "/img/icons8-cardápio-50.png");
+    }
+
+    menu.classList.toggle('active');
+});
+let count = 1;
+document.getElementById("radio1").ariaChecked = true;
+
+setInterval(function () {
+    nextImage();
+}, 6000)
+function nextImage() {
+    count++;
+    if (count > 4) {
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
