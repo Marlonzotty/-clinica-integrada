c0b1c293ccdbadfcdd203b6793bf42f70b09e40d
Marlonzotty <112110890+Marlonzotty@users.noreply.github.com>
2023-07-25 22:46:06 -0300

    meu

--- menunav.js
+++ menunav.js
@@ -4,10 +4,10 @@ var iconMenu = document.querySelector('nav .menu-icon img');
 
 menuBar.addEventListener('click', function () {
 
-    if (iconMenu.getAttribute("src") == '/img/icons8-cardápio-50.png') {
-        iconMenu.setAttribute("src", "/img/icons8-cardápio-50.png");
+    if (iconMenu.getAttribute("src") == '/img/listras.png') {
+        iconMenu.setAttribute("src", "/img/listras.png");
     } else {
-        iconMenu.setAttribute("src", "/img/icons8-cardápio-50.png");
+        iconMenu.setAttribute("src", "/img/listras.png");
     }
 
     menu.classList.toggle('active');
